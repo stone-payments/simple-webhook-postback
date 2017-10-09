73393ab491f0353cb94effc2e1e4d8799d947cc3
Artur Pianor <artur.pianor@gmail.com>
2017-10-09 12:39:45 -0300

    Subindo fonte

---
+++ server.js
@@ -0,0 +1,119 @@
+//----------------------//
+//-----VARIAVEIS--------//
+//----------------------//
+const server = require('express')()
+const bodyParser = require('body-parser');
+const moment = require('moment');
+const server_port = 4000;
+
+var count = 0;
+var lastDateTimeReceived;
+var firstDateTimeReceived;
+
+//----------------------//
+//-----ENDPOINTS--------//
+//----------------------//
+
+///
+///EndPoint para Develop
+///
+server.post('/dev/', function (req, res) {
+
+	console.log("200 - Requisicao Recebida :" + req.originalUrl);
+	count++;
+	res.sendStatus(200);
+})
+
+///
+///EndPoint para Staging
+///
+server.post('/stg/', function (req, res) {
+
+	console.log("200 - Requisicao Recebida :" + req.originalUrl);
+	count++;
+	
+	res.sendStatus(200);
+})
+
+///
+///EndPoint para teste de carga
+///
+server.post('/sdx/', function (req, res) {
+	
+	console.log("200 - Requisicao Recebida :" + req.originalUrl);
+	lastDateTimeReceived = moment().format("YYYY-MM-DDThh:mm:ss");
+	console.log(lastDateTimeReceived);
+	
+	if(count == 0){
+		firstDateTimeReceived = lastDateTimeReceived;
+	}	
+	
+	count++;	
+	
+	res.sendStatus(200);
+})
+
+///
+///Limpa o contador
+///
+server.get('/clear/', function (req, res) {
+	console.log("202 - Requisicao Recebida :" + req.originalUrl);
+	count = 0;
+	res.sendStatus(202);
+})
+
+///
+///Retorna o timeStamp da última requisição
+///
+server.get('/timelapse/', function (req, res) {
+	console.log("202 - Requisicao Recebida :" + req.originalUrl);
+	res.writeHead(202, { "Content-Type": "application/json" });
+	
+	res.end(lastDateTimeReceived);
+})
+
+///
+///Apenas para teste e/ou KeepAlive
+///
+server.get('/', function (req, res) {
+	
+	console.log("200 - Requisicao Recebida :" + req.originalUrl);
+	res.writeHead(202, { "Content-Type": "application/json" });
+	var n = count;
+	var json = JSON.stringify({
+		count: n
+	});
+	res.end(n.toString());
+})
+
+///
+///Apenas para teste e/ou KeepAlive
+///
+server.get('/status/', function (req, res) {
+
+	console.log("202 - Requisicao Recebida ");
+	console.log(moment().format("YYYY-MM-DDThh:mm:ss:SSS"));
+	
+	res.end("I'M ALIIIVE!!!");
+})
+
+
+//----------------------//
+//-------FUNCOES--------//
+//----------------------//
+
+// Configura o servidor para receber json no body
+server.use(bodyParser.json());
+
+// Executa a cada Requisicao.
+server.all('*', function (req, res, next) {
+	console.log("Requisicao Recebida :" + req.originalUrl);
+	next();
+})
+server.use(function (req, res, next) {
+	console.log("Requisicao Recebida :" + req.originalUrl);
+	next();
+})
+//Declara porta
+server.listen(server_port,
+	() => console.log('Servidor Iniciado na porta: ' + server_port));
